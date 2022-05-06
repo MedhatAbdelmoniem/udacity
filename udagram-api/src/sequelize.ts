6ce57b306e651a63390193890e62eac161ad446a
MedhatAbdelmoniem <87497679+MedhatAbdelmoniem@users.noreply.github.com>
2022-05-06 05:12:04 +0200

    .

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -6,7 +6,6 @@ export const sequelize = new Sequelize({
   password: config.password,
   database: config.database,
   host: config.host,
-  port: 1234,
 
   dialect: "postgres",
   storage: ":memory:",

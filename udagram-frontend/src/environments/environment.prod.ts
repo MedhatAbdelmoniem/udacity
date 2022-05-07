952b8aa099b8c115229e528aae777d3c43e0ec42
MedhatAbdelmoniem <87497679+MedhatAbdelmoniem@users.noreply.github.com>
2022-05-07 12:59:10 +0200

    fixing an error

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udacity-env.eba-mipt427m.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
